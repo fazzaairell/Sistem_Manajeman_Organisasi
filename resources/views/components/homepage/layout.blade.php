59af858f26dfcdcb4e04df1994c697b087e466cf
roymss <roymartinsilaban199@gmail.com>
2025-12-30 00:14:43 +0700

    Resolve Conflict

--- resources/views/components/homepage/layout.blade.php
+++ resources/views/components/homepage/layout.blade.php
@@ -22,18 +22,6 @@
     <!-- Alpine.js v3 -->
     <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 
-    <script>
-        // di halaman lain
-        navigate("/", { state: { scrollTo: "galeri" } });
-
-        // di homepage, cek state dan scroll
-        useEffect(() => {
-            if (location.state?.scrollTo === "galeri") {
-                document.getElementById("galeri").scrollIntoView({ behavior: "smooth" });
-            }
-        }, []);
-    </script>
-
 
 </body>
 
