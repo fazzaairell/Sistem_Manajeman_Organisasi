799b90023ceb1a5be29554ef0a5e1ac34c916d97
Rifkifa <rifkifajrianakbar01@gmail.com>
2025-12-31 11:22:28 +0700

    feat: make hero section responsive

--- resources/views/components/homepage/hero.blade.php
+++ resources/views/components/homepage/hero.blade.php
@@ -1,7 +1,6 @@
 <div id="home" class="relative flex justify-center items-center mt-16 px-4">
     <div class="w-full lg:w-[70%] flex flex-col lg:flex-row rounded-tr-[60px] lg:rounded-tr-[150px] overflow-hidden">
 
-        <!-- Text Section -->
         <div class="w-full lg:w-1/2 p-6 sm:p-10 space-y-4">
             <p class="px-4 py-2 bg-gradient-to-r from-purple-500 to-indigo-600 w-fit rounded-2xl text-xs text-white">
                 Mengenal Kami lebih jauh disini!
@@ -23,7 +22,6 @@
             </button>
         </div>
 
-        <!-- Image Section -->
         <div class="w-full lg:w-1/2 h-64 sm:h-80 lg:h-auto">
             <div
                 class="w-full h-full bg-[url('/public/image/page.png')] bg-cover bg-center rounded-bl-none lg:rounded-bl-full">
